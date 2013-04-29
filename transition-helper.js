fcce1b7ed9827a520f3d876e3b3ccba064bb8f83
Bacon <github@iambacon.co.uk>
2013-04-29 12:56:24 +0100

    Added comments to functions

--- transition-helper.js
+++ transition-helper.js
@@ -4,6 +4,8 @@
 		easing: 'easing'
 	}
 
+    // Function: transition
+    // Adds a transition to an element.
 	$.fn.transition = function (properties, options) {
         options = $.extend({}, defaults, options);
         properties['webkitTransition'] = 'all ' + options.duration + 'ms ' + options.easing;
@@ -13,6 +15,8 @@
         $(this).css(properties);
     };
 
+    // Function: noTransition
+    // Stops an animation by removing it.
     $.fn.noTransition = function (properties) {
         properties['webkitTransition'] = 'none';
         properties['transition'] = 'none';
@@ -21,6 +25,9 @@
         $(this).css(properties);
     };
 
+    // Function: redraw
+    // Forces a redraw of the element.
+    // Required when a browser batches commands that should be run seperately.
     $.fn.redraw = function () {
         $(this).each(function ()
         {
@@ -28,6 +35,8 @@
         });
     };
 
+	// jQuery.support.transition
+	// Checks for browser support for CSS3 transitions.  Returns boolean.
     $.support.transitions = (function () {
     	var s = document.createElement('p').style, // 's' for style
         v = ['ms', 'O', 'Moz', 'Webkit']; // 'v' for vendor
